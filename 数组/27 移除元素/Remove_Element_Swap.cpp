65a45ad94c5ef65e4b93b389090ec0ad642add25
刘子源 <1500012963@pku.edu.cn>
2019-12-28 17:33:29 +0800

    Create Remove_Element_Swap.cpp

---
+++ 数组/27 移除元素/Remove_Element_Swap.cpp
@@ -0,0 +1,21 @@
+/*
+  Time: 0ms
+  Memory: 8.4MB
+*/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        int i=0;
+        while(i<n)
+        {
+            if(nums[i]==val)
+            {
+                nums[i]=nums[n-1];
+                n--;
+            }
+            else i++;
+        }
+        return n;
+    }
+};
