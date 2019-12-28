e92837725af613eb0a7e1062f93a279ddfed4055
刘子源 <1500012963@pku.edu.cn>
2019-12-28 17:26:23 +0800

    Update Remove_Element_Two_Pointers.cpp

--- 数组/27 移除元素/Remove_Element_Two_Pointers.cpp
+++ 数组/27 移除元素/Remove_Element_Two_Pointers.cpp
@@ -1,16 +1,14 @@
 /*
-	Time: 8ms
-	Memory: 8.6MB
+	Time: 4ms
+	Memory: 8.5MB
 */
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int fast;
         int slow = 0;
-        for(fast = 0;fast < nums.size();fast++){
+        for(int fast = 0;fast < nums.size();fast++){
             if(nums[fast]!=val){
-                nums[slow] = nums[fast];
-                slow++;
+                nums[slow++] = nums[fast];
             }
         }
         return slow;
