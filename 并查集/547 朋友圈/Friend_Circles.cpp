3241e3919b625d669745226a12da47a3866bf9ca
刘子源 <1500012963@pku.edu.cn>
2019-12-29 20:45:32 +0800

    Create Friend_Circles.cpp

---
+++ 并查集/547 朋友圈/Friend_Circles.cpp
@@ -0,0 +1,60 @@
+/*
+	Time: 28ms
+	Memory: 10.6MB
+*/
+class Solution {
+public:
+    int parent[210];
+    //查找该节点属于哪一朋友圈
+    int Find(int x)
+    {
+        while(x!=parent[x])
+        {
+            x = parent[x];
+        }
+        return x;
+    }
+    //合并节点：设置为同一朋友圈
+    void Union(int a,int b)
+    {
+        int pa = Find(a);
+        int pb = Find(b);
+        if(pa!=pb)
+        {
+            parent[pa] = pb;
+        }
+    }
+    //最开始,每个节点各自为一朋友圈
+    void init()
+    {
+        for(int i=0;i<210;i++)
+        {
+            parent[i] = i;
+        }
+    }
+    
+    int findCircleNum(vector<vector<int>>& M) {
+        init();
+        //对N个学生两两做判断
+        for(int i = 0;i < M.size();i++)
+        {
+            for(int j=i+1;j < M.size();j++)
+            {
+                if(M[i][j]==1)
+                {
+                    Union(i,j);
+                }
+            }
+        }
+        //一次遍历找到所有祖先节点，即为朋友圈的个数
+        int circle = 0;
+        for(int i = 0;i < M.size();i++)
+        {
+            if(i == parent[i])
+            {
+                circle++;
+            }
+        }
+        return circle;
+    }
+};
